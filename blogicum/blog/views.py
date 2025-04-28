0b899abd81bfd8cfcab9c48f5062590be9bdbaa3
Senia Babenko <senia.babenko@gmail.com>
2025-04-28 06:10:04 +0300

    19

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -17,11 +17,18 @@ def category_posts(request, category_slug):
 
     posts = category.posts.filter(
         is_published=True,
-        pub_date__lte=timezone.now()).order_by('-pub_date')
+        pub_date__lte=timezone.now(),
+        category__is_published=True
+    ).order_by('-pub_date')
     if not posts.exists():
         posts = []
     if request.user == category.posts.first().author:
         posts = category.posts.all()
+    if request.user.is_authenticated:
+        posts = category.posts.filter(
+            posts(is_published=True) | posts(author=request.user),
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')    
     paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
